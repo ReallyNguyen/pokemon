e450626610ac1eec1420e97f20123697fc94112f
ReallyNguyen <jnguyen188@my.bcit.ca>
2026-01-11 22:25:37 -0800

    added api in readme and testing in app.js

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ tailwindcss https://tailwindcss.com/
 motion https://motion.dev/docs/react
 
 ### `npm start`
+
+## API's Used
+
+[PokeApi](https://pokeapi.co/)
+
+[Pokemon TCG Api](https://pokemontcg.io/)
--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from 'react'
+import axios from 'axios';
+
 
 function App() {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://pokeapi.co/api/v2/pokemon/ditto');
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (isLoading) return <h3>Loading data...</h3>;
+  if (!data) return <h3>No data found</h3>;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
+        <h1>{data.name.toUpperCase()}</h1>
+        <p>Height: {data.height}</p>
+        <p>Base Experience: {data.base_experience}</p>
+
+        <h3>Abilities:</h3>
+        <ul>
+          {data.abilities.map((abilityObj, index) => (
+            <li key={index}>{abilityObj.ability.name}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
 }
+
 
 export default App;
